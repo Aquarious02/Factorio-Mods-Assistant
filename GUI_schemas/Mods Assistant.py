bee5915bd1732463041ebdedff7ab3af581c1a0b
Aquarious <aquarious0696@gmail.com>
2020-10-13 16:50:58 +0300

    Selection connecting

--- GUI_schemas/Mods Assistant.py
+++ GUI_schemas/Mods Assistant.py
@@ -14,7 +14,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.mods_handler = ModsHandler()
 
-        self.mods_in_table = []
+        self.mods_in_table = {}
 
         #
         # menu
@@ -24,42 +24,91 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.action_save_as.triggered.connect(self.save_as)
         self.ui.action_make_copy.triggered.connect(self.make_copy)
 
+        self.ui.checkBox_choose_all.clicked.connect(self.choose_all)
+        self.ui.checkBox_enable.clicked.connect(self.change_state)
+
     def update_table(self):
         self.ui.tableWidget.setRowCount(len(self.mods_in_table))
-        for mode_index, mode in enumerate(self.mods_in_table):
-            mode_name_item = QtWidgets.QTableWidgetItem()
-            mode_name_item.setText(mode['name'])
-            mode_name_item.setTextAlignment(QtCore.Qt.AlignCenter)
-            self.ui.tableWidget.setItem(mode_index, 0, mode_name_item)
+        for mode_index, mode in enumerate(self.mods_in_table.items()):
+            name, state = mode
+            mode_item = QtWidgets.QTableWidgetItem()
+            mode_item.setText(name)
+            # mode_name_item.setTextAlignment(QtCore.Qt.AlignCenter)
+            mode_item.setTextAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignVCenter)
 
-            mode_state_item = QtWidgets.QTableWidgetItem()
-            if mode['enabled']:
-                mode_state_item.setCheckState(QtCore.Qt.Checked)
+            if state:
+                mode_item.setCheckState(QtCore.Qt.Checked)
             else:
-                mode_state_item.setCheckState(QtCore.Qt.Unchecked)
+                mode_item.setCheckState(QtCore.Qt.Unchecked)
 
-            self.ui.tableWidget.setItem(mode_index, 1, mode_state_item)
+            # if mode['name'] == 'base':
+            #     mode_item.setVisible(False)
+
+            self.ui.tableWidget.setItem(mode_index, 0, mode_item)
+
+    def update_changes(self):
+        """Writes mods from table to mods_handler"""
+        for row in range(self.ui.tableWidget.rowCount()):
+            item = self.ui.tableWidget.item(row, 0)
+            self.mods_handler.mods[item.text()] = item.checkState() == 2
+
+    def choose_all(self):
+        """
+        Select all items in table
+        """
+        if self.sender().isChecked():
+            self.ui.tableWidget.selectAll()
+        else:
+            self.ui.tableWidget.clearSelection()
+        self.ui.tableWidget.setFocus()
+
+        # for row in range(self.ui.tableWidget.rowCount()):
+        #     self.ui.tableWidget.setItemSelected(self.ui.tableWidget.item(row, 0), True)
+
+    def change_state(self):
+        """
+        Change state of selected items
+        """
+        items = self.ui.tableWidget.selectedItems()
+
+        new_state = self.sender().isChecked()
+        for item in items:
+            if new_state:
+                item.setCheckState(QtCore.Qt.Checked)
+            else:
+                item.setCheckState(QtCore.Qt.Unchecked)
+
+        self.ui.tableWidget.setFocus()
 
     def open(self):
         """
         Open mods file
         """
         file_name = QtWidgets.QFileDialog.getOpenFileName(self, directory=self.mods_handler.mods_path or 'mods')[0]
-        # file_name = QtWidgets.QFileDialog.getOpenFileName(self)[0]
         if file_name != '':
             self.mods_handler.mods_file_path = file_name  # it was "empty" because of initialization above
-            self.mods_in_table = self.mods_handler.open_json()
+            self.mods_handler.update_mods()
+            self.mods_in_table = self.mods_handler.mods
             self.update_table()
 
     def save(self):
-        pass
+        self.update_changes()
+        if self.mods_handler.mods_file_path is None:
+            self.save_as()
+        else:
+            self.mods_handler.save_mods()
 
     def save_as(self):
-        pass
+        """
+        Opens fileDialog to ask where to save
+        """
+        self.update_changes()
+        file_path = QtWidgets.QFileDialog.getSaveFileName(self, directory=self.mods_handler.mods_path or 'mods')[0]
+        if file_path != '':
+            self.mods_handler.save_mods(file_path=file_path)
 
     def make_copy(self):
         pass
-
 
     @classmethod
     def start(cls):
--- Mods_assistant_lib.py
+++ Mods_assistant_lib.py
@@ -5,9 +5,10 @@ import os
 class ModsHandler:
     def __init__(self, mods_path: str = None, mods_file_path: str = None):
         try:
+            self.mods = {}
             self.mods_path = mods_path or fr'{os.environ["appdata"]}\Factorio\mods'
             self._mods_file_path = mods_file_path or self.mods_path + r'\mod-list.json'
-            self.mods = {'current': self.open_json()}
+            self.update_mods()
         except FileNotFoundError:
             self.mods_path, self._mods_file_path = None, None
             print("Can't find mods in AppData...")
@@ -23,52 +24,72 @@ class ModsHandler:
         if self.mods_path is None:
             self.mods_path = header
         self._mods_file_path = value
+        self.update_mods()
 
-
-
-
-    @property
-    def mods_names(self):
+    @staticmethod
+    def mods_list_to_dict(list_from_mods_file) -> dict:
         """
-        Returns dict of mod with pair "mod_name": enabled(bool)
+        Returns dict of mods with pairs: "mod_name": enabled(bool)
         :return:
         """
         mods_names = {}
-        for mod in self.mods['current']:
+        for mod in list_from_mods_file:
             mods_names[mod['name']] = mod['enabled']
         return mods_names
 
-    def open_json(self, file_path: str = None) -> list:
+    @staticmethod
+    def mods_dict_to_list(dict_with_mods) -> list:
+        """
+        Creates list of mods to save in .json.
+        :return: [{"name": "mod1", "enabled": true}, {"name": "mod2", "enabled": false}, ...]
+        """
+        mods = []
+        for mod_name, mod_state in dict_with_mods.items():
+            mods.append({'name': mod_name, 'enabled': mod_state})
+        return mods
+
+    def update_mods(self):
+        """
+        Updates mods from file
+        """
+        if self.mods_file_path is not None:
+            with open(self.mods_file_path, 'r') as f:
+                self.mods = self.mods_list_to_dict(json.load(f)['mods'])
+        else:
+            raise FileNotFoundError
+
+    @staticmethod
+    def open_json(file_path: str) -> list:
         """
         Opens json file
-        :param file_path:
+        :param file_path: open file with path from handler if None
         :return: json file in list
         """
-        file_path = file_path or self.mods_file_path
         with open(file_path, 'r') as f:
             mods = json.load(f)['mods']
         return mods
 
-    def save_json(self, file_path: str = None, file_name: str = 'mod-list_new'):
+    def save_mods(self, file_path: str = None, file_name: str = None):
         """
-        saves .json file
+        saves mods in .json file
         """
         file_path = file_path or self.mods_file_path
-        file_path = file_path.replace('mod-list', file_name)
+        if file_name is not None:
+            file_path = file_path.replace('mod-list', file_name)
 
         with open(file_path, 'w') as f:
-            json.dump({'mods': self.mods}, f)
+            json.dump({'mods': self.mods_dict_to_list(self.mods)}, f)
 
     def make_base(self):
         """
         Turn off all modes to load faster. Creates "mod-list-initial" - copy of current state of mods-list
         """
-        self.save_json(file_name='mod-list-initial')
-        for mod in self.mods['current']:
+        self.save_mods(file_name='mod-list-initial')
+        for mod in self.mods:
             if mod['name'] != 'base' and mod['enabled']:
                 mod['enabled'] = False
 
-        self.save_json(file_name='mod-list')
+        self.save_mods(file_name='mod-list')
 
     def merge_modes(self):
         """
@@ -76,11 +97,9 @@ class ModsHandler:
         :return:
         """
         new_modes = self.open_json()
-        old_modes_names = self.mods_names
+        old_modes_names = self.mods_dict
         for new_mode in new_modes:
             if new_mode['name'] not in old_modes_names:
                 self.mods.append({new_mode['name']: new_mode['enabled']})
 
-        self.save_json(file_name='mod-list')
-
-
+        self.save_mods(file_name='mod-list')
